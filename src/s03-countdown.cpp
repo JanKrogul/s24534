58f02331bf479dc4532e4da730629e130b30e6e7
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-19 01:36:02 +0200

    add new file

---
+++ src/s03-countdown.cpp
@@ -0,0 +1,16 @@
+x
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main( int argc, char * argv[]){
+	if (argc == 0){
+	return 1;
+	}
+	for (auto i = stoi(argv[1]); i > -1; --i )
+	{
+	cout << i  << "\n";
+	}
+}
+
+
