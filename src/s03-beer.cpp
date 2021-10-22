8a0c5e20cb8ede699406346edebc30c9665bca3b
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-22 23:58:55 +0200

    add new file

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -4,11 +4,14 @@ int i, n;
 auto main(int argc, char* argv[]) -> int
 {
 	n = 5;
-	for (i = n; i <= 0; i--)
+	for (i = n; i >= 1; i--)
 	{
 	cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
 	cout << "Take one down, pass it around, " ;
 	}
+	cout << "No more bottles of beer on the wall, no morre bottles of beer\n";
+	cout << "Go to the store and buy some more, 99 bottles of beer on the wall.../n";
+
 
 
 return 0;
