4c3bb7d7e723960986cc901786ce6acca6d56d53
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-23 00:24:50 +0200

    add new file

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -3,14 +3,18 @@ using namespace std;
 int i, n;
 auto main(int argc, char* argv[]) -> int
 {
-	n = 5;
+	n = 99;
+	if (argc > 1)
+	{
+	n = stoi(argv[1]);
+	}
 	for (i = n; i >= 1; i--)
 	{
 	cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
 	cout << "Take one down, pass it around, " ;
 	}
 	cout << "No more bottles of beer on the wall, no morre bottles of beer\n";
-	cout << "Go to the store and buy some more, 99 bottles of beer on the wall.../n";
+	cout << "Go to the store and buy some more, 99 bottles of beer on the wall..." << "\n";
 
 
 
