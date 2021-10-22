61d5fc1673f281e423a5a6f58062a84999923d2a
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-22 23:51:50 +0200

    add new file

---
+++ src/s03-beer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int i, n;
+auto main(int argc, char* argv[]) -> int
+{
+	n = 5;
+	for (i = n; i <= 0; i--)
+	{
+	cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
+	cout << "Take one down, pass it around, " ;
+	}
+
+
+return 0;
+}
+
