960d1a91e34f650c52adf5818191529bf8956ec4
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-27 22:15:10 +0200

    add new file

--- src/s01-hello-name.cpp
+++ src/s01-hello-name.cpp
@@ -4,7 +4,7 @@
 auto main() -> int
 {
 	auto name = std::string{};
-	std::getline(std::cin, name);
+	std::getline(std::cin, name); // wpisywanie przez użytkownika wartości i zapisanie je od zmiennej name 
 
 	std::cout << "Hello, " << name << "!\n";
 
