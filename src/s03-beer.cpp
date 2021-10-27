3a49b6c7a502f9e31e4a2fa7f996c9685756ac55
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-27 22:16:17 +0200

    add new file

--- src/s03-beer.cpp
+++ src/s03-beer.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 using namespace std;
-int i, n;
+int i, n; //zaznaczenie żeby zmienne "i" oraz "n" były liczbami całkowitymi
 auto main(int argc, char* argv[]) -> int
 {
 	n = 99;
-	if (argc > 1)
+	if (argc > 1) //jezeli nie ma wpisanego argumentu podczas odpalania piku to pobiera zmienną n napisaną w kodzie powyżej i zamienia ją na liczbę stałą
 	{
-	n = stoi(argv[1]);
+	n = stoi(argv[1]);//pobieranie pierwszego argumentu wpisanego podczas odpalania programu
 	}
-	for (i = n; i >= 1; i--)
+	for (i = n; i >= 1; i--)//dla zmiennej "i" równej "n" , jeżeli zmienna "i" jest większa lub równa od liczby 1 to  zrób operacje -1
 	{
 	cout << i << " bottles of beer on the wall, " << i << " bottles of beer.\n";
 	cout << "Take one down, pass it around, " ;
 	}
 	cout << "No more bottles of beer on the wall, no morre bottles of beer\n";
 	cout << "Go to the store and buy some more, 99 bottles of beer on the wall..." << "\n";
-
-
-
 return 0;
 }
 
