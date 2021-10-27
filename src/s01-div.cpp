c60ec9da26d6ed80b596564813bbd28694c7e1c7
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-27 22:15:03 +0200

    add new file

--- src/s01-div.cpp
+++ src/s01-div.cpp
@@ -1,11 +1,11 @@
-#include <iostream>    // for std::cout, std::cerr, and std::cin
+#include <iostream>   
 #include <string>
 
-auto main(int argc, char* argv[]) -> int
+auto main(int argc, char* argv[]) -> int //pobranie od razu z wiersza poleceń zmiennych a i b
 {
-        auto const a = std::stoi(argv[1]);
-        auto const b = std::stoi(argv[2]);
-        std::cout << (a / b) << "\n";
+        auto const a = std::stoi(argv[1]);// pobiera pierwszą wartość podaną jako pierwszy argument podczas odpalania prorgamu
+        auto const b = std::stoi(argv[2]);// pobiera drugą wartość podaną jako drugi argument w terminalu podczas odaplania programu
+        std::cout << (a / b) << "\n"; //wypisanie operacji dzielenia stałej a i b wpisanych przez użytkownika wcześniej
         return 0;
 }
 
