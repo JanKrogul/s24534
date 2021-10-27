bf2bb6e25ae0fddf54522e9854fab48c4f7513e7
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-27 22:16:25 +0200

    add new file

--- src/s03-countdown.cpp
+++ src/s03-countdown.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main( int argc, char * argv[]){
-	if (argc == 0){
+	if (argc == 0){//jezeli liczba argumentów wpisanych przez użytkownika jest równa 0  zwraca błąd logiczny
 	return 1;
 	}
 	for (auto i = stoi(argv[1]); i > -1; --i )
