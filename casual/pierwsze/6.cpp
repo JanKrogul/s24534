c574dc1801b8d8a9c5092455a99e2a6dddc24166
JanKrogul <s24534@pjwstl.edu.pl>
2021-10-28 04:13:14 +0200

    add new file

---
+++ casual/pierwsze/6.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+auto main (int agrc, char* argv[]) -> int
+{
+	auto const a = std::stoi(argv[1]);
+	auto const b = std::stoi(argv[2]);
+
+	if (a > b){
+	std::cout << a << " > " << b << "\n";
+	} else if (a < b){
+	std::cout << a << " < " << b << "\n";
+	} else if (a == b){
+	std::cout << a << " = " << b << "\n";
+	}
+	return 0;
+}
