376ec7c2d3f266e30ff171c0f22e1d8b952988aa
JanKrogul <s24534@pjwstl.edu.pl>
2021-11-12 02:29:01 +0100

    add new file

--- casual/drugie/10.cpp
+++ casual/drugie/10.cpp
@@ -3,10 +3,10 @@ auto main (int argc, char* argv[]) -> int
 {
 	auto a = std::stoi(argv[1]);
 std::cout << "Silnia z " << a ;
-int silnia=1,z=1;
+int silnia=1,b=1;
 	do{
-		silnia = silnia * z;
-		z++;
+		silnia = silnia * b;
+		b++;
 		a--;
 	} while(0 < a);
 
