ef5b674de137ee9801e08f7c8728d9afb3fe9cd5
JanKrogul <s24534@pjwstl.edu.pl>
2021-11-12 11:13:10 +0100

    add new file

---
+++ casual/trzecie/3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+auto main (int argc, char* argv[]) -> int
+{
+        auto const a = std::stoi(argv[1]);//std::stoi(argv[1]);//rozmiar tablicy
+	int const b = 50;
+	int tab[b]={};
+	srand( time(NULL));//daje losowe liczby na podstawie pobranego czasu
+		for(int i=0; i < a ; i++){
+			tab[i] = (std::rand() % 50 ) + 0 ;
+		}
+std::cout << tab[0] << "\n";
+std::cout << tab[1] << "\n";
+
+return 0;
+}
