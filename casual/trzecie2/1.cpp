bc3239e993fff8bded62ac4d9f75a3e521f793d1
JanKrogul <s24534@pjwstl.edu.pl>
2021-11-16 20:25:18 +0100

    add new file

---
+++ casual/trzecie2/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <array>
+
+int main(){
+std::array < char, 127 > ASCIItable;
+
+	for(int i = 33; i <= 126; i++){
+		ASCIItable[i] = i;
+		std::cout << ASCIItable[i] << "\n";
+	}
+return 0;
+
+}
