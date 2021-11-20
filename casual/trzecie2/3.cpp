75042b64fb95fba547e032ed2e14a479b87d6f96
JanKrogul <s24534@pjwstl.edu.pl>
2021-11-20 07:07:37 +0100

    add new file

---
+++ casual/trzecie2/3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+int main()
+{
+	std::vector <int> b;
+	int a, wielkosc;
+	do{
+		std::cout << "Podaj " << b.size() + 1 << " liczbe: ";
+		std::cin>>a;
+		b.push_back(a);
+		wielkosc = b.size();
+	}while(a != 0);
+	for(int i = 0 ;i < wielkosc - 1; i++){
+		for(int j = 0; j < wielkosc - 2; j++){
+			if(b[j] > b[j+1]){
+				std::swap(b[j], b[j+1]);//swap zamienia miejscami
+			}
+		}
+	}
+	for(int x = 0; x < wielkosc -1;x++){
+		std::cout << b[x] << "\n";
+	}
+
+	std::cout<<"\n";
+	return 0;
+}
