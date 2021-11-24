0463a7058d1b8313a310284f5bb48cd33f585ace
JanKrogul <s24534@pjwstl.edu.pl>
2021-11-24 04:55:50 +0100

    add new file

--- casual/trzecie3/1.cpp
+++ casual/trzecie3/1.cpp
@@ -1,1 +1,27 @@
 #include <iostream>
+
+void pierwszy()
+{
+	std::cout << "--------------------\n";
+}
+void drugi()
+{
+	std::cout << "I***   ***   ***   I\n";
+}
+void trzeci()
+{
+	std::cout << "I   ***   ***   ***I\n";
+}
+int main(){
+	pierwszy();
+	drugi();
+	drugi();
+	trzeci();
+	trzeci();
+	drugi();
+	drugi();
+	trzeci();
+	trzeci();
+	pierwszy();
+return 0;
+}
